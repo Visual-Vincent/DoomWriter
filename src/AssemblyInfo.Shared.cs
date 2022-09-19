779aa3ef7c9122c1d170920931ee9d0dbfdd5ff1
Visual-Vincent <github-vv@mydoomsite.com>
2022-09-19 22:25:25 +0200

    Added AssemblyVersion attribute

--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -5,4 +5,5 @@ using System.Resources;
 [assembly: AssemblyCompany("My DOOM site")]
 [assembly: AssemblyProduct("Doom Writer")]
 [assembly: AssemblyCopyright("Copyright © Visual Vincent 2014-2022")]
+[assembly: AssemblyVersion("3.0.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
