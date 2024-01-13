23143e05d3c22dcf539b200bc106c3b5150eec9c
Visual-Vincent <github-vv@mydoomsite.com>
2024-01-13 14:08:28 +0100

    Updated copyright year

--- src/AssemblyInfo.Shared.cs
+++ src/AssemblyInfo.Shared.cs
@@ -4,6 +4,6 @@ using System.Resources;
 
 [assembly: AssemblyCompany("My DOOM site")]
 [assembly: AssemblyProduct("Doom Writer")]
-[assembly: AssemblyCopyright("Copyright © Visual Vincent 2014-2023")]
+[assembly: AssemblyCopyright("Copyright © Visual Vincent 2014-2024")]
 [assembly: AssemblyVersion("3.0.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
